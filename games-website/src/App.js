21f5bab6a17c59408583bee973e18b6adf380943
Austin Vigo <avigo@uci.edu>
2020-02-27 23:35:35 -0800

    Cleaned up the code
    Cleaned up main by removing unecessary components

--- games-website/src/App.js
+++ games-website/src/App.js
@@ -5,21 +5,15 @@ import { connect } from 'react-redux';
 
 // Components
 import { 
-  HelloWorldLabel,
-  ChangeLabelButton
+
 } from './Components';
 
 const App = () => {
   return (
     <div>
-      <HelloWorldLabel/>
-      <ChangeLabelButton/>
+
     </div>
   );
-}
-
-const buttonPressed = () => {
-  console.log("Test");
 }
 
 export default App;
