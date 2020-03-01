4431ac1f4aa4576ce7912cd9775df74728e0b759
Austin Vigo <avigo@uci.edu>
2020-03-01 01:34:38 -0800

    installed dependency
    installed dependency to be able to connect to api

--- games-website/src/App.js
+++ games-website/src/App.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, {Component} from 'react';
+
+// To create a socket that connects client to the server
+import socketIOClient from "socket.io-client";
 
 // Redux imports
 import { connect } from 'react-redux';
@@ -8,12 +11,28 @@ import {
 
 } from './Components';
 
-const App = () => {
-  return (
-    <div>
+class App extends Component {
 
-    </div>
-  );
+  state = {
+    response: false,
+    endpoint: "http://localhost:5000",
+    response: null
+  };
+
+  constructor(props) {
+    super(props);
+    const { endpoint } = this.state;
+    const socket = socketIOClient(endpoint);
+    socket.on("FromAPI", data => this.setState({response: data }));
+  }
+  
+  render() {
+    return (
+      <div>
+          <h1>It is working</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
